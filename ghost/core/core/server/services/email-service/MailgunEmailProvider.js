96124c99e9787b490303094565924270fcae9b79
Sam Lord <hello@samlord.me>
2025-11-04 16:49:13 +0000

    WIP: Added fallback sending domain
    ref
    [GVA-588](https://linear.app/ghost/issue/GVA-588/add-the-ability-to-send-from-a-fallback-domain)

--- ghost/core/core/server/services/email-service/MailgunEmailProvider.js
+++ ghost/core/core/server/services/email-service/MailgunEmailProvider.js
@@ -90,6 +90,7 @@ class MailgunEmailProvider {
             html,
             plaintext,
             from,
+            domain,
             replyTo,
             emailId,
             recipients,
@@ -107,6 +108,7 @@ class MailgunEmailProvider {
                 plaintext,
                 from,
                 replyTo,
+                domain,
                 id: emailId,
                 track_opens: !!options.openTrackingEnabled,
                 track_clicks: !!options.clickTrackingEnabled
@@ -180,7 +182,7 @@ class MailgunEmailProvider {
 
     /**
      * Returns the configured delay between batches in milliseconds
-     * 
+     *
      * @returns {number}
      */
     getTargetDeliveryWindow() {
